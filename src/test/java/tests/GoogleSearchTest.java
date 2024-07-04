4338e3ace28d6a01c79b4f3560e55258a4b5e188
Nazarii Riabcheniuk <n251096@gmail.com>
2024-07-04 22:48:25 +0300

    + a failing testcase + screenshots on test's ending

--- src/test/java/tests/GoogleSearchTest.java
+++ src/test/java/tests/GoogleSearchTest.java
@@ -8,21 +8,25 @@ import static config.constants.*;
 public class GoogleSearchTest extends CommonConditions{
 
     @Test
-    public void openGoogleSearchPage() {
+    public void openGoogleSearchPage() throws InterruptedException {
         System.out.println("Browser: " + System.getProperty("browser"));
 
         driver.get(GOOGLE_SEARCH_PAGE);
         String actualTitle = driver.getTitle();
+
+        Thread.sleep(1_000);
 
         Assert.assertEquals(GOOGLE_PAGE_TITLE, actualTitle);
     }
 
     @Test
-    public void openGoogleSearchPageFail() {
+    public void openGoogleSearchPageFail() throws InterruptedException {
         System.out.println("Browser: " + System.getProperty("browser"));
 
         driver.get(GOOGLE_SEARCH_PAGE);
         String actualTitle = driver.getTitle();
+
+        Thread.sleep(1_000);
 
         Assert.assertEquals("GOOGLE_PAGE_TITLE", actualTitle);
     }
